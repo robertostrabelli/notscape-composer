4c7d5b18ed63b83a73feac8e366a1f33b1f10c2e
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2019-12-08 23:43:18 +0000

    Create 2019-12-08_Hello-world.php

---
+++ 2019-12-08_Hello-world.php
@@ -0,0 +1,36 @@
+<?php
+define('cabeca', TRUE); define('configura', TRUE); $canonico = ($_SERVER['REQUEST_URI']);
+$datamod = date ("c", getlastmod());
+
+
+$pgRobos="index,follow"; // SEARCH BOTS INSTRUCTION - SEE https://developers.google.com/search/reference/robots_meta_tag#directives_1
+$pgRoboGoogle=""; // INSTRUCTION ONLY FOR GOOGLEBOT
+
+$pgModelo="texto"; // PAGE TYPE - "texto" for regular post with author info on footer, "textofixo" for pages without footer
+
+$pgTitulo="Hello World!"; // POST TITLE - recommended to use same as filename
+
+$pgChaves="first, post, awsome, blog"; // META TAGS
+
+$pgDescricao="This is my first post on my new blog"; // SUMMARY OR DESCRIPTION
+
+$AutorPost="John Smith"; // POST AUTHOR'S NAME
+
+$AutorPostBio="about.php"; // AUTHOR BIO PAGE
+
+$AutorPostJob="Boss"; // AUTHOR'S JOB - NOT MANDATORY
+
+$AutorPostCompany="Strabelli.com"; AUTHOR'S COMPANY - NOT MANDATORY
+
+$datapub="2019-12-03T00:00:00-03:00"; // PUB DATE - MANDATORY IN THIS FORMAT 2019-12-31T23:59:59-03:00
+
+
+require ('001.php'); ?>
+<?php // CONTEUDO INICIO  ?><?php echo ('
+
+
+<p>Hi, my name is John Smith and this is my first post on my new awsome personal blog.</p>
+
+
+'); ?><?php // CONTEUDO FIM  ?>
+<?php define('rodape', TRUE); require ('003.php'); ?>
