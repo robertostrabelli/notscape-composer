949001ca9f743dd668f6c2fe5a6ed4b373aea3fc
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2019-12-29 18:24:16 +0000

    Update privacy.php

--- privacy.php
+++ privacy.php
@@ -1,26 +1,25 @@
 <?php
-define('cabeca', TRUE); 
-define('configura', TRUE); 
+define('cabeca', true);
 $canonico = ($_SERVER['REQUEST_URI']);
 $datamod = date ("c", getlastmod());
 
 // SEARCH BOTS INSTRUCTION - SEE https://developers.google.com/search/reference/robots_meta_tag#directives_1
-$pgRobos = "noindex,follow"; 
+$pgRobos = "noindex,follow";
 
 // INSTRUCTION FOR GOOGLEBOT ONLY
 $pgRoboGoogle = "";
 
 // PAGE TYPE - "texto" FOR REGULAR POSTS WITH AUTHOR INFO FOOTER, "textofixo" FOR PAGES WITHOUT FOOTER
-$pgModelo = "textofixo"; 
+$pgModelo = "textofixo";
 
 // TITLE - RECOMMENDED TO USE SAME OR SIMILAR AS FILENAME
-$pgTitulo = "Privacy policy of this site"; 
+$pgTitulo = "Privacy policy of this site";
 
 // META TAGS - 3 OR 4 STRONG KEYWORDS WILL SUFFICE
 $pgChaves = "privacy, data, policy, cookies";
 
 // SUMMARY OR DESCRIPTION - IT IS RECOMMENDED TO USE A SHORT PHRASE
-$pgDescricao = "how we do about privacy"; 
+$pgDescricao = "how we do about privacy";
 
 // PAGE AUTHOR'S NAME
 $AutorPost = "John";
@@ -37,9 +36,9 @@ $AutorPostCompany = "JS Inc.";
 // PUB DATE - MANDATORY IN THIS FORMAT 2019-12-31T23:59:59-01:00 - SEE README.MD FOR INSTRUCTIONS
 $datapub = "2019-12-03T16:49:05-03:00";
 
-require ('001.php'); ?>
-<?php // CONTENT START  ?><?php echo ('
+require_once __DIR__.'/001.php';
 
+$conteudo = '
 
 <h4>Who we are?</h4>
 <p>John Smith Inc.</p>
@@ -50,6 +49,8 @@ require ('001.php'); ?>
 <h4>What rights do you have to the data recorded here</h4>
 <p>All!</p>
 
+';
 
-'); ?> <?php // CONTENT END  ?>
-<?php define('rodape', TRUE); require ('003.php'); ?>
+echo html_entity_decode($conteudo, ENT_HTML5);
+define('rodape', true);
+require_once __DIR__.'/003.php';?>
