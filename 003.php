c76057b54cade57dc64721918b4aac5c023f2ace
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2019-12-29 18:17:33 +0000

    Update 003.php

--- 003.php
+++ 003.php
@@ -1,18 +1,20 @@
-<?php defined('rodape') or die(' ');
-echo('
+<?php
+defined('rodape') or die ("Erro" . htmlentities($Erro2, ENT_QUOTES, "utf-8"));
+$Erro2 = 'Erro';
+$conteudor1 = '
 </div>
 <footer class="metapost">
-<p class="momento"><small><time datetime="' . $datapub . '" itemprop="datePublished">' . $datapub . '</time>
-<meta itemprop="dateModified" content="' . $datamod . '">
-<meta itemprop="description" content="' . $pgDescricao . '">
+<p class="momento"><small><time datetime="' . htmlentities($datapub, ENT_QUOTES, "utf-8") . '" itemprop="datePublished">' . htmlentities($datapub, ENT_QUOTES, "utf-8") . '</time>
+<meta itemprop="dateModified" content="' . htmlentities($datamod, ENT_QUOTES, "utf-8") . '">
+<meta itemprop="description" content="' . htmlentities($pgDescricao, ENT_QUOTES, "utf-8") . '">
 <span role="separator">|</span>
 <span itemprop="author" itemscope itemtype="https://schema.org/Person">
-<a rel="author" itemprop="url" href="' . $AutorPostBio . '" title="View author biography"><span itemprop="name">' . $AutorPost . '</span></a>
-<meta itemprop="jobTitle" content="' . $AutorPostJob . '">
-<meta itemprop="worksFor" content="' . $AutorPostCompany . '">
+<a rel="author" itemprop="url" href="' . htmlentities($AutorPostBio, ENT_QUOTES, "utf-8") . '" title="View author biography"><span itemprop="name">' . htmlentities($AutorPost, ENT_QUOTES, "utf-8") . '</span></a>
+<meta itemprop="jobTitle" content="' . htmlentities($AutorPostJob, ENT_QUOTES, "utf-8") . '">
+<meta itemprop="worksFor" content="' . htmlentities($AutorPostCompany, ENT_QUOTES, "utf-8") . '">
 </span>
 <span itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<meta itemprop="name" content="' . $PublisherSite . '">
+<meta itemprop="name" content="' . htmlentities($PublisherSite, ENT_QUOTES, "utf-8") . '">
 <span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
 <meta itemprop="url" content="android-chrome-512x512.png">
 <meta itemprop="width" content="512">
@@ -20,15 +22,17 @@ echo('
 </span>
 </span>
 <span role="separator">|</span>
-Tags: <span itemprop="keywords">'); ?>
-<?php // divisao palavras chave ?>
-<?php $pgChaves = explode(',', $pgChaves);
+Tags: <span itemprop="keywords">';
+
+echo html_entity_decode($conteudor1, ENT_HTML5, "utf-8");
+
+$pgChaves = explode(',', $pgChaves);
 foreach($pgChaves as $link) { // if the site was installed in a directory and not in the domain or subdomain root, put the folder name like this /mysite/ after ' sign in the line below right after $EnderecoSite
-echo '<a rel="category tag" href="https://www.qwant.com/?q='.$link.'+site:' . $EnderecoSite . '" title="tag search" target="_blank">'.$link.'</a>';
-}?>
-<?php // divisao palavras chave ?>
-<?php
-echo('</span>
+$conteudor2 = '<a rel="category tag" href="https://www.qwant.com/?q='.htmlentities($link, ENT_QUOTES, "utf-8").'+site:' . htmlentities($EnderecoSite, ENT_QUOTES, "utf-8") . '" title="tag search" target="_blank">'.htmlentities($link, ENT_QUOTES, "utf-8").'</a>';
+}
+echo html_entity_decode($conteudor2, ENT_HTML5, "utf-8");
+$conteudor3 = '
+</span>
 </small></p>
 </footer>
 <span itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
@@ -41,8 +45,10 @@ echo('</span>
 </main>
 <footer class="roda">
 <p><a href="#top">[Top]</a></p>
-' . $FooterInfo . '
+' . htmlentities($FooterInfo, ENT_HTML5, "utf-8") . '
 </footer>
 </body>
 </html>
-'); ?>
+';
+echo html_entity_decode($conteudor3, ENT_HTML5, "utf-8");
+?>
