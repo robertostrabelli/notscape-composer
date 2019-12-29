bc7c8e47be1cff350dc09bf4b2e38a4d09f7ec54
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2019-12-29 18:20:58 +0000

    Update copyright.php

--- copyright.php
+++ copyright.php
@@ -1,6 +1,5 @@
 <?php
-define('cabeca', TRUE); 
-define('configura', TRUE); 
+define('cabeca', true);
 $canonico = ($_SERVER['REQUEST_URI']);
 $datamod = date ("c", getlastmod());
 
@@ -11,7 +10,7 @@ $pgRobos = "noindex,follow";
 $pgRoboGoogle = "";
 
 // PAGE TYPE - "texto" FOR REGULAR POSTS WITH AUTHOR INFO FOOTER, "textofixo" FOR PAGES WITHOUT FOOTER
-$pgModelo = "textofixo"; 
+$pgModelo = "textofixo";
 
 // TITLE - RECOMMENDED TO USE SAME OR SIMILAR AS FILENAME
 $pgTitulo = "Copyright";
@@ -37,15 +36,17 @@ $AutorPostCompany = "JS Inc.";
 // PUB DATE - MANDATORY IN THIS FORMAT 2019-12-31T23:59:59-01:00 - SEE README.MD FOR INSTRUCTIONS
 $datapub = "2019-12-03T16:49:05-03:00";
 
-require ('001.php'); ?>
-<?php // CONTENT START  ?><?php echo ('
+require_once __DIR__.'/001.php';
 
+$conteudo = '
 
 <p><strong><em>Attention to my rules:</em></strong></p>
 <p>Plagiarism is not cool</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna etiam tempor orci eu lobortis. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Tincidunt dui ut ornare lectus sit amet est placerat in. Tortor id aliquet lectus proin nibh nisl condimentum id venenatis. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Imperdiet proin fermentum leo vel orci. Amet consectetur adipiscing elit ut aliquam. Mauris augue neque gravida in fermentum. Porttitor leo a diam sollicitudin tempor id eu.</p>
 <p>Massa eget egestas purus viverra accumsan in nisl. Eu non diam phasellus vestibulum. A condimentum vitae sapien pellentesque habitant. Dolor sit amet consectetur adipiscing elit. Sem fringilla ut morbi tincidunt augue interdum velit euismod in. Sit amet mauris commodo quis imperdiet. Lorem ipsum dolor sit amet consectetur adipiscing. Tincidunt arcu non sodales neque sodales ut etiam sit. Donec et odio pellentesque diam volutpat commodo sed egestas. Neque ornare aenean euismod elementum nisi quis eleifend. Ut eu sem integer vitae justo eget magna fermentum iaculis. Ultricies mi eget mauris pharetra et ultrices. Ac turpis egestas maecenas pharetra convallis posuere. Bibendum enim facilisis gravida neque convallis a. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris.</p>
 
+';
 
-'); ?> <?php // CONTENT END  ?>
-<?php define('rodape', TRUE); require ('003.php'); ?>
+echo html_entity_decode($conteudo, ENT_HTML5);
+define('rodape', true);
+require_once __DIR__.'/003.php';?>
