2b5cafd6810ced74926eb8a5faff7f44ce234ec4
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2019-12-30 23:51:48 +0000

    Update search.php

--- search.php
+++ search.php
@@ -37,9 +37,10 @@ $AutorPostCompany = "JS Inc.";
 $datapub = "2019-12-03T16:49:05-03:00";
 
 require_once __DIR__.'/001.php';
+echo <<<HTML
 
-$conteudo = '
 
+<!-- BODY TEXT START -->
 <script src="assets/js/search.js"></script>
 <form method="GET" action="https://www.qwant.com" id="form" onsubmit="addSiteSearch()">
 <input type="text" name="q" id="query" size="31" minlength="3" maxlength="40" value="" aria-label="Search through site content" required>
@@ -47,9 +48,9 @@ $conteudo = '
 <input type="submit" name="btnG" value="Go!">
 </form>
 <p><small>Powered by <a href="https://www.qwant.com" target="_blank">Qwant</a></small></p>
+<!-- BODY TEXT END -->
 
-';
 
-echo html_entity_decode($conteudo, ENT_HTML5);
+HTML;
 define('rodape', true);
 require_once __DIR__.'/003.php';?>
