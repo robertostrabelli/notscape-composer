4232858179dcd2eae03e516d1051fdd552494248
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2019-12-31 21:40:19 +0000

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -51,36 +51,33 @@ echo <<<HTML
 
 <!-- BODY TEXT START -->
 <p>Sample portfolio content with css class table</p>
-<hr />
-<div class="tabportifolio">
-<div class="tabportifolioimg">
-<figure><img src="assets/img/2019/sample.jpg" alt="Image sample" /><figcaption>http://mrg.bz/f44d4a</figcaption></figure>
-</div>
-<div class="tabportifoliodes"><h5>One of my jobs</h5><h6>2007 - 2009</h6>
+
+<div class="tabport">
+<div class="tabport-txt"><h5>One of my jobs</h5><h6>2007 - 2009</h6>
 <p>You can use this format style for your portfolio or make your own css.</p>
 </div>
-</div>
-<hr />
-<div class="tabportifolio">
-<div class="tabportifolioimg">
+<div class="tabport-img">
 <figure><img src="assets/img/2019/sample.jpg" alt="Image sample" /><figcaption>http://mrg.bz/f44d4a</figcaption></figure>
 </div>
-<div class="tabportifoliodes"><h5>Another one of my jobs</h5><h6>2010 - 2011</h6>
+</div>
+<div class="tabport">
+<div class="tabport-txt"><h5>Another one of my jobs</h5><h6>2010 - 2011</h6>
 <p>The base code is:</p>
 <pre><small><code>
-&lt;div class="tabportifolio"&gt;
-&lt;div class="tabportifolioimg"&gt;
-IMAGE HERE
-&lt;/div&gt;
-&lt;div class="tabportifoliodes"&gt;
+&lt;div class="tabport"&gt;
+&lt;div class="tabport-txt"&gt;
 TEXT HERE
 &lt;/div&gt;
+&lt;div class="tabport-img"&gt;
+IMAGE HERE
 &lt;/div&gt;
-&lt;hr /&gt;
+&lt;/div&gt;
 </code></small></pre>
 </div>
+<div class="tabport-img">
+<figure><img src="assets/img/2019/sample.jpg" alt="Image sample" /><figcaption>http://mrg.bz/f44d4a</figcaption></figure>
 </div>
-<hr />
+</div>
 <!-- BODY TEXT END -->
 
 
