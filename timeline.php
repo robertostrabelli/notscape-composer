3dce8d4f50e13edc33b6304e2ea839e4854efd43
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2020-01-01 13:13:33 +0000

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -60,7 +60,7 @@ $data = str_replace('-',' ',$data);
 $conteudot2 = '<tr><td><a href='. htmlentities($NomedoArquivo, ENT_QUOTES, "utf-8") . '>' . htmlentities($titulo, ENT_QUOTES, "utf-8") . '</a></td><td>' . htmlentities($data, ENT_QUOTES, "utf-8") . '</td></tr>';
 echo html_entity_decode($conteudot2, ENT_HTML5);
  }
-$conteudot3 = '<tfoot><tr><td>Tip: You can place a link here pointing to a zip archive with old posts if the list gets too long. Or delete it</td><td>ARCHIVE.ZIP</td></tr></tfood></table>';
+$conteudot3 = '<tfoot><tr><td>Tip: You can place a link here pointing to a zip archive with old posts if the list gets too long. Or delete it</td><td>ARCHIVE.ZIP</td></tr></tfoot></table>';
 
 echo html_entity_decode($conteudot3, ENT_HTML5);
 define('rodape', true);
