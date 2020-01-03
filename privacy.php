d76b43949c1aa58e937a8cedf0e11ef2303fc992
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2020-01-03 11:06:46 +0000

    Update privacy.php

--- privacy.php
+++ privacy.php
@@ -1,53 +1,23 @@
 <?php
 define('cabeca', true);
-$canonico = ($_SERVER['REQUEST_URI']);
+$Uri = ($_SERVER['REQUEST_URI']);
 $datamod = date ("c", getlastmod());
-
-// CONTENT RATING
-// Safe For Kids               -CONTENT FOR CHILDREN ONLY
-// 14 Years                    -FOR MINIMUM AGE
-// General                     -FOR EVERYBODY
-// Mature                      -ADULTS ONLY
-// Restricted
-// RTA-5042-1996-1400-1577-RTA -RESTRICTED TO ADULTS
-$Classifica = "General";
-
-// SEARCH BOTS INSTRUCTION - SEE https://developers.google.com/search/reference/robots_meta_tag#directives_1
-$pgRobos = "noindex,follow";
-
-// INSTRUCTION FOR GOOGLEBOT ONLY
-$pgRoboGoogle = "";
-
-// PAGE TYPE - "texto" FOR REGULAR POSTS WITH AUTHOR INFO FOOTER, "textofixo" FOR PAGES WITHOUT FOOTER
-$pgModelo = "textofixo";
-
-// TITLE - RECOMMENDED TO USE SAME OR SIMILAR AS FILENAME
-$pgTitulo = "Privacy policy of this site";
-
-// META TAGS - 3 OR 4 STRONG KEYWORDS WILL SUFFICE - NO SPACE BETWEEN WORDS
-$pgChaves = "privacy,data,policy,cookies";
-
-// SUMMARY OR DESCRIPTION - IT IS RECOMMENDED TO USE A SHORT PHRASE
-$pgDescricao = "how we do about privacy";
-
-// PAGE AUTHOR'S NAME
-$AutorPost = "John";
-
-// AUTHOR BIO PAGE - USE ABOUT.PHP FOR UNIQUE AUTHOR SITE
-$AutorPostBio = "about.php"; // NOT MANDATORY
-
-// AUTHOR'S JOB - NOT MANDATORY
-$AutorPostJob = "boss";
-
-// AUTHOR'S COMPANY - NOT MANDATORY
-$AutorPostCompany = "JS Inc.";
-
-// PUB DATE - MANDATORY IN THIS FORMAT 2019-12-31T23:59:59-01:00 - SEE README.MD FOR INSTRUCTIONS
-$datapub = "2019-12-03T16:49:05-03:00";
-
+$PageRating = "General";
+$RobotsParameters = "noindex,follow";
+$GoogleBotDirectives = "";
+$PageTemplate = "pagina";
+$PageTitle = "Privacy policy";
+$PageKeywords = "privacy,data,policy,cookies";
+$PageDescription = "";
+$PageAuthor = "John";
+$PageAuthorBio = "about.php";
+$PageAuthorJob = "Boss";
+$PageAuthorCompany = "JS Inc.";
+$DatePub = "2019-12-03T16:49:05-03:00";
+$Comments="off";
+$ShowMeta="off";
 require_once __DIR__.'/001.php';
 echo <<<HTML
-
 
 <!-- BODY TEXT START -->
 <h4>Who we are?</h4>
@@ -59,7 +29,6 @@ echo <<<HTML
 <h4>What rights do you have to the data recorded here</h4>
 <p>All!</p>
 <!-- BODY TEXT END -->
-
 
 HTML;
 define('rodape', true);
