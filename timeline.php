c957a4c0626338151f8ffeb363b701ce23be16fc
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2020-01-03 11:13:22 +0000

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -1,50 +1,21 @@
 <?php
 define('cabeca', true);
-$canonico = ($_SERVER['REQUEST_URI']);
+$Uri = ($_SERVER['REQUEST_URI']);
 $datamod = date ("c", getlastmod());
-
-// CONTENT RATING
-// Safe For Kids               -CONTENT FOR CHILDREN ONLY
-// 14 Years                    -FOR MINIMUM AGE
-// General                     -FOR EVERYBODY
-// Mature                      -ADULTS ONLY
-// Restricted
-// RTA-5042-1996-1400-1577-RTA -RESTRICTED TO ADULTS
-$Classifica = "General";
-
-// SEARCH BOTS INSTRUCTION - SEE https://developers.google.com/search/reference/robots_meta_tag#directives_1
-$pgRobos = "noindex,follow";
-
-// INSTRUCTION FOR GOOGLEBOT ONLY
-$pgRoboGoogle = "";
-
-// PAGE TYPE - "timeline" ONLY FOR POSTS LIST. "texto" FOR REGULAR POSTS WITH AUTHOR INFO FOOTER, "textofixo" FOR PAGES WITHOUT FOOTER
-$pgModelo = "timeline";
-
-// TITLE - RECOMMENDED TO USE SAME OR SIMILAR AS FILENAME
-$pgTitulo = "All Posts";
-
-// META TAGS - 3 OR 4 STRONG KEYWORDS WILL SUFFICE - NO SPACE BETWEEN WORDS
-$pgChaves = "posts,timeline,texts,list";
-
-// SUMMARY OR DESCRIPTION - IT IS RECOMMENDED TO USE A SHORT PHRASE
-$pgDescricao = "This is a list of all blog posts";
-
-// PAGE AUTHOR'S NAME
-$AutorPost = "John";
-
-// AUTHOR BIO PAGE - USE ABOUT.PHP FOR UNIQUE AUTHOR SITE - NOT NECESSARY HERE
-$AutorPostBio = "about.php";
-
-// AUTHOR'S JOB - NOT MANDATORY
-$AutorPostJob = "boss";
-
-// AUTHOR'S COMPANY - NOT MANDATORY
-$AutorPostCompany = "JS Inc.";
-
-// PUB DATE - MANDATORY IN THIS FORMAT 2019-12-31T23:59:59-01:00 - SEE README.MD FOR INSTRUCTIONS
-$datapub = "2019-12-03T16:49:05-03:00";
-
+$PageRating = "General";
+$RobotsParameters = "noindex,follow";
+$GoogleBotDirectives = "";
+$PageTemplate = "timeline";
+$PageTitle = "All posts";
+$PageKeywords = "posts,timeline,texts,list";
+$PageDescription = "";
+$PageAuthor = "John";
+$PageAuthorBio = "about.php";
+$PageAuthorJob = "Boss";
+$PageAuthorCompany = "JS Inc.";
+$DatePub = "2019-12-03T16:49:05-03:00";
+$Comments="off";
+$ShowMeta="off";
 require_once __DIR__.'/001.php';
 
 $conteudot1 = '<table class="timeline"><thead><tr><td>Article</td><td>Year Month Day</td></tr></thead>';
