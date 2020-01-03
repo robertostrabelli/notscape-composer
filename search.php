090e03234e37aeff338308cf26b7e22c21a44938
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2020-01-03 11:10:02 +0000

    Update search.php

--- search.php
+++ search.php
@@ -1,64 +1,33 @@
 <?php
 define('cabeca', true);
-$canonico = ($_SERVER['REQUEST_URI']);
+$Uri = ($_SERVER['REQUEST_URI']);
 $datamod = date ("c", getlastmod());
-
-// CONTENT RATING
-// Safe For Kids               -CONTENT FOR CHILDREN ONLY
-// 14 Years                    -FOR MINIMUM AGE
-// General                     -FOR EVERYBODY
-// Mature                      -ADULTS ONLY
-// Restricted
-// RTA-5042-1996-1400-1577-RTA -RESTRICTED TO ADULTS
-$Classifica = "General";
-
-// SEARCH BOTS INSTRUCTION - SEE https://developers.google.com/search/reference/robots_meta_tag#directives_1
-$pgRobos = "noindex,nofollow";
-
-// INSTRUCTION FOR GOOGLEBOT ONLY
-$pgRoboGoogle = "";
-
-// PAGE TYPE - RECOMMENDED "entrada" FOR SEARCH PAGE. "texto" FOR REGULAR POSTS WITH AUTHOR INFO FOOTER, "textofixo" FOR PAGES WITHOUT FOOTER
-$pgModelo = "entrada";
-
-// TITLE - RECOMMENDED TO USE SAME OR SIMILAR AS FILENAME
-$pgTitulo = "DO A SEARCH IN MY BLOG";
-
-// META TAGS - 3 OR 4 STRONG KEYWORDS WILL SUFFICE - NO SPACE BETWEEN WORDS
-$pgChaves = "search";
-
-// SUMMARY OR DESCRIPTION - IT IS RECOMMENDED TO USE A SHORT PHRASE
-$pgDescricao = "";
-
-// PAGE AUTHOR'S NAME
-$AutorPost = "John";
-
-// AUTHOR BIO PAGE - USE ABOUT.PHP FOR UNIQUE AUTHOR SITE - NOT NECESSARY HERE
-$AutorPostBio = "about.php";
-
-// AUTHOR'S JOB - NOT MANDATORY
-$AutorPostJob = "Boss";
-
-// AUTHOR'S COMPANY - NOT MANDATORY
-$AutorPostCompany = "JS Inc.";
-
-// PUB DATE - MANDATORY IN THIS FORMAT 2019-12-31T23:59:59-01:00 - SEE README.MD FOR INSTRUCTIONS
-$datapub = "2019-12-03T16:49:05-03:00";
-
+$PageRating = "General";
+$RobotsParameters = "noindex,nofollow";
+$GoogleBotDirectives = "";
+$PageTemplate = "entrada";
+$PageTitle = "Do a search";
+$PageKeywords = "search";
+$PageDescription = "";
+$PageAuthor = "John";
+$PageAuthorBio = "about.php";
+$PageAuthorJob = "Boss";
+$PageAuthorCompany = "JS Inc.";
+$DatePub = "2019-12-03T16:49:05-03:00";
+$Comments="off";
+$ShowMeta="off";
 require_once __DIR__.'/001.php';
 echo <<<HTML
-
 
 <!-- BODY TEXT START -->
 <script src="assets/js/search.js"></script>
 <form method="GET" action="https://www.qwant.com" id="form" onsubmit="addSiteSearch()">
 <input type="text" name="q" id="query" size="31" minlength="3" maxlength="40" value="" aria-label="Search through site content" required>
-<input type="hidden" name="l" value="pt">
+<input type="hidden" name="l" value="en">
 <input type="submit" name="btnG" value="Go!">
 </form>
 <p><small>Powered by <a href="https://www.qwant.com" target="_blank">Qwant</a></small></p>
 <!-- BODY TEXT END -->
-
 
 HTML;
 define('rodape', true);
