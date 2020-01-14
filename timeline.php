ba362975e644f5c9b3c1203625a733d79d2b57e5
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2020-01-14 22:14:56 +0000

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -16,6 +16,7 @@ $PageAuthorCompany = "JS Inc.";
 $DatePub = "2019-12-03T16:49:05-03:00";
 $Comments="off";
 $ShowMeta="off";
+$Image="android-chrome-512x512.png";
 require_once __DIR__.'/001.php';
 
 $conteudot1 = '<table class="timeline"><thead><tr><td>Article</td><td>Year Month Day</td></tr></thead>';
@@ -28,7 +29,7 @@ $titulo = $parte[1];
 $data = $parte[0];
 $titulo = str_replace('-',' ',$titulo);
 $data = str_replace('-',' ',$data);
-$conteudot2 = '<tr><td><a href='. htmlentities($NomedoArquivo, ENT_QUOTES, "utf-8") . '>' . htmlentities($titulo, ENT_QUOTES, "utf-8") . '</a></td><td>' . htmlentities($data, ENT_QUOTES, "utf-8") . '</td></tr>';
+$conteudot2 = '<tr><td><a href="'. htmlentities($NomedoArquivo, ENT_QUOTES, "utf-8") . '">' . htmlentities($titulo, ENT_QUOTES, "utf-8") . '</a></td><td>' . htmlentities($data, ENT_QUOTES, "utf-8") . '</td></tr>';
 echo html_entity_decode($conteudot2, ENT_HTML5);
  }
 $conteudot3 = '<tfoot><tr><td>Tip: You can place a link here pointing to a zip archive with old posts if the list gets too long. Or delete it</td><td>ARCHIVE.ZIP</td></tr></tfoot></table>';
