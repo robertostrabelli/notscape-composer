69474731c8ba473bf13276b92aea5206cf1b820d
Roberto <52572531+robertostrabelli@users.noreply.github.com>
2020-01-14 22:34:29 +0000

    Update 003.php

--- 003.php
+++ 003.php
@@ -16,17 +16,17 @@ $conteudor1 = '
 <span itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
 <meta itemprop="name" content="' . htmlentities($PublisherSite, ENT_QUOTES, "utf-8") . '">
 <span itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-<meta itemprop="url" content="android-chrome-512x512.png">
-<meta itemprop="width" content="512">
-<meta itemprop="height" content="512">
+<meta itemprop="url" content="' . htmlentities($PublisherLogoSite, ENT_QUOTES, "utf-8") . '">
+<meta itemprop="width" content="' . htmlentities($PublisherLogoWidthSite, ENT_QUOTES, "utf-8") . '">
+<meta itemprop="height" content="' . htmlentities($PublisherLogoHeightSite, ENT_QUOTES, "utf-8") . '">
 </span>
 </span>
 <span role="separator">&bull;</span>
 Tags: <span itemprop="keywords">';
 echo html_entity_decode($conteudor1, ENT_HTML5, "utf-8");
 $PageKeywords = explode(',', $PageKeywords);
-foreach($PageKeywords as $link) { // if the site was installed in a directory and not in the domain or subdomain root, put the folder name like this /mysite/ between ' and " signs in the line below after $UrlSite
-echo '<span class="palavrachave"><a rel="category tag" href="https://www.qwant.com/?q='.htmlentities($link, ENT_QUOTES, "utf-8").'+site:' . htmlentities($UrlSite, ENT_QUOTES, "utf-8") . '" title="tag search" target="_blank">'.htmlentities($link, ENT_QUOTES, "utf-8").'</a></span>';
+foreach($PageKeywords as $link) {
+echo '<span class="palavrachave"><a rel="category tag" href="https://www.qwant.com/?q='.htmlentities($link, ENT_QUOTES, "utf-8").'+site:' . htmlentities($UrlBaseSite, ENT_QUOTES, "utf-8") . htmlentities($UrlDirectorySite, ENT_QUOTES, "utf-8") . '" title="tag search" target="_blank">'.htmlentities($link, ENT_QUOTES, "utf-8").'</a></span>';
 }
 $conteudor3 = '
 </span>
@@ -46,6 +46,42 @@ $conteudor3 = '
 <p><a href="#top">[Top]</a></p>
 ' . htmlentities($FooterInfo, ENT_HTML5, "utf-8") . '
 </footer>
+
+<script type="application/ld+json">
+  {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    "headline": "' . htmlentities($PageTitle, ENT_QUOTES, "utf-8") . '",
+    "image": "' . htmlentities($Image, ENT_QUOTES, "utf-8") . '",
+    "publisher": {
+      "@type": "Organization",
+      "name": "' . htmlentities($PublisherSite, ENT_QUOTES, "utf-8") . '",
+      "url": "' . htmlentities($PublisherUrlSite, ENT_QUOTES, "utf-8") . '",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "' . htmlentities($PublisherLogoSite, ENT_QUOTES, "utf-8") . '",
+        "width": "' . htmlentities($PublisherLogoWidthSite, ENT_QUOTES, "utf-8") . '",
+        "height": "' . htmlentities($PublisherLogoHeightSite, ENT_QUOTES, "utf-8") . '"
+      }
+    },
+    "url": "' . htmlentities($UrlBaseSite, ENT_QUOTES, "utf-8") . htmlentities($Uri, ENT_QUOTES, "utf-8") . '",
+    "datePublished": "' . htmlentities($DatePub, ENT_QUOTES, "utf-8") . '",
+    "dateCreated": "' . htmlentities($DatePub, ENT_QUOTES, "utf-8") . '",
+    "dateModified": "' . htmlentities($datamod, ENT_QUOTES, "utf-8") . '",
+    "description": "' . htmlentities($PageDescription, ENT_QUOTES, "utf-8") . '",
+    "author": {
+      "@type": "Person",
+      "name": "' . htmlentities($PageAuthor, ENT_QUOTES, "utf-8") . '",
+      "url": "' . htmlentities($PageAuthorBio, ENT_QUOTES, "utf-8") . '"
+    },
+    "mainEntityOfPage": {
+      "@type": "WebPage",
+      "@id": "' . htmlentities($UrlBaseSite, ENT_QUOTES, "utf-8") . htmlentities($UrlDirectorySite, ENT_QUOTES, "utf-8") . '"
+    }
+  }
+</script>
+
+
 </body>
 </html>
 ';
